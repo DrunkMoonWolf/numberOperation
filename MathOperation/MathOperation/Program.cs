d8027f325c658aa7aa29b0e11cf21acfcdd9c395
DrunkMoonWolf <zemerovanastya2017@gmail.com>
2022-01-12 18:38:10 +0500

    change sum, add space

--- MathOperation/MathOperation/Program.cs
+++ MathOperation/MathOperation/Program.cs
@@ -15,7 +15,7 @@ namespace MathOperation
             switch (operation)
             {
                 case "+":
-                    Console.WriteLine(a+b);
+                    Console.WriteLine(a + b);
                     break;
                 case "-":
 
