d830f0dbec01d515aa14a0a604e796b50fb97279
DrunkMoonWolf <zemerovanastya2017@gmail.com>
2022-01-12 17:21:12 +0500

    first change

---
+++ MathOperation/MathOperation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MathOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число a");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Какую операцию выполнить? ('+', '-', '*', '/')");
+            string operation = Console.ReadLine();
+            switch (operation)
+            {
+                case "+":
+
+                    break;
+                case "-":
+
+                    break;
+                case "*":
+
+                    break;
+                case "/":
+
+
+                    break;
+                default:
+                    Console.WriteLine("Такого варианта нет!");
+                    break;
+            }
+        }
+    }
+}
