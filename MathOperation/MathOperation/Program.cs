0a47bb98dbb4b3bcbce43398f6c01ed0e1cffe07
DrunkMoonWolf <zemerovanastya2017@gmail.com>
2022-01-21 20:02:21 +0500

    add lol

--- MathOperation/MathOperation/Program.cs
+++ MathOperation/MathOperation/Program.cs
@@ -10,6 +10,7 @@ namespace MathOperation
             double a = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Введите число b");
             double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("lol");
             Console.WriteLine("Какую операцию выполнить? ('+', '-', '*', '/')");
             string operation = Console.ReadLine();
             switch (operation)
